8728a451470f3d0c6a5dae623d08e6304a8aa5c1
giorgiorosson <giorgiorosson268@gmail.com>
2026-01-14 13:18:36 +0100

    fix errori, non suona ancora

--- LandTiger-Template-main/Source/music/music.c
+++ LandTiger-Template-main/Source/music/music.c
@@ -1,5 +1,5 @@
 #include "music.h"
-
+#include "LPC17xx.h"
 #include "../timer/timer.h"
 
 void playNote(NOTE note)
@@ -7,11 +7,29 @@ void playNote(NOTE note)
 	if(note.freq != pause)
 	{
 		reset_timer(0);
-		init_timer(0, note.freq);
+		
+		// --- 1. CALCOLO FREQUENZA ---
+		// Convertiamo Hz in Tick del processore (25MHz)
+		// Formula: 25.000.000 / (Freq * 2)
+		uint32_t ticks = 25000000 / (note.freq * 2);
+		
+		// --- 2. INIT TIMER 0 (Speaker) ---
+		// Parametri: TimerNum=0, Prescaler=0, MatchReg=0, Config=3(Int+Reset), Value=ticks
+		init_timer(0, 0, 0, 3, ticks);
+		
+		// --- 3. CONFIGURAZIONE PIN DAC (P0.26) ---
+		// Impostiamo P0.26 come uscita analogica per suono pulito
+		LPC_PINCON->PINSEL1 &= ~(3 << 20); // Pulisce
+		LPC_PINCON->PINSEL1 |= (2 << 20);  // Imposta AOUT
+		
 		enable_timer(0);
 	}
 	reset_timer(1);
-	init_timer(1, note.duration);
+	
+	// --- 4. INIT TIMER 1 (Durata) ---
+	// Parametri: TimerNum=1, Prescaler=0, MatchReg=0, Config=3, Value=duration
+	init_timer(1, 0, 0, 3, note.duration);
+	
 	enable_timer(1);
 }
 
